5be2ba0f315d4f3c9ada2107142fe893f3b36c23
shashankms9 <104433795+shashankms9@users.noreply.github.com>
2025-06-06 19:49:05 +0530

    Add src/backend/api/services/huggingface.ts

---
+++ src/backend/api/services/huggingface.ts
@@ -0,0 +1,35 @@
+import { BpaServiceObject } from "../engine/types";
+import axios from 'axios'
+
+export class HuggingFace {
+
+    private _endpoint
+    
+    constructor(endpoint : string){
+        this._endpoint = endpoint
+    }
+
+    public process = async (input : BpaServiceObject, index : number) : Promise<BpaServiceObject> => {
+        const body = {
+            text : input.data,
+            modelId : input.serviceSpecificConfig.modelId
+        }
+        console.log(JSON.stringify(body))
+        const result = await axios.post(`${this._endpoint}/api/analyze`, body)
+        const results = input.aggregatedResults
+        results["huggingFaceNer"] = result
+        input.resultsIndexes.push({index : index, name : "huggingFaceNer", type : "huggingFaceNer"})
+        return {
+            data : result.data,
+            label : "huggingFaceNer",
+            bpaId : input.bpaId,
+            filename: input.filename,
+            pipeline: input.pipeline,
+            type : "huggingFaceNer",
+            aggregatedResults : results,
+            resultsIndexes : input.resultsIndexes,
+            vector: input.vector
+        }
+    }
+
+}
