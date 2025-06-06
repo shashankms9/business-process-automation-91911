00573d3586755145b3ccc8862059d15194d377a1
shashankms9 <104433795+shashankms9@users.noreply.github.com>
2025-06-06 19:52:29 +0530

    Add src/frontend/src/Components/Dialogs/LanguageCustomNerDialog.jsx

---
+++ src/frontend/src/Components/Dialogs/LanguageCustomNerDialog.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Dialog } from '@fluentui/react-northstar';
+import { Input } from '@fluentui/react-northstar';
+
+
+export default function LanguageCustomNerDialog(props) {
+
+    const [projectName, setProjectName] = useState(null)
+    const [deploymentName, setDeploymentName] = useState(null)
+
+    const onDialogSave = (event) => {
+        console.log(event)
+        const newOption = props.currentOption
+        newOption.serviceSpecificConfig = { projectName: projectName, deploymentName: deploymentName }
+        props.setHideDialog(true)
+        props.addItemToPipeline(newOption)
+    }
+
+    const onDialogCancel = () => {
+        props.setHideDialog(true)
+    }
+
+    const onProjectNameDialogChange = (event, newValue) => {
+        setProjectName(newValue.value)
+    }
+
+    const onDeploymentNameDialogChange = (event, newValue) => {
+        setDeploymentName(newValue.value)
+    }
+
+
+    return (
+        <Dialog
+            header="Language Custom NER"
+            content={<>
+                    <Input label="Project name" labelPosition="inline" value={projectName} onChange={onProjectNameDialogChange} style={{marginBottom: "10px"}}/>
+                    <Input label="Deployment name" labelPosition="inline" value={deploymentName} onChange={onDeploymentNameDialogChange} />
+                        </>}
+            open={!props.hideDialog}
+            cancelButton="Cancel"
+            confirmButton="Submit"
+            onConfirm={onDialogSave}
+            onCancel={onDialogCancel}
+        />
+    )
+} 
